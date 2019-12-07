4a03e4ba61248c90c8dcd62b24067084935ab4f7
Stian Onarheim <43683888+Feqzz@users.noreply.github.com>
2019-12-07 11:14:25 +0100

    Got the parser to work.

--- php/season.php
+++ php/season.php
@@ -24,46 +24,112 @@ class season
 
         //Getting race information and making a race object.
 
-        $season = $MRData->RaceTable->attributes()->{'season'};
-        $round = $MRData->RaceTable->attributes()->{'round'};
-        $raceName = $MRData->RaceTable->Race->RaceName;
-        $circuitId = $MRData->RaceTable->Race->Circuit->attributes()->{'circuitId'};
-        $circuitName = $MRData->RaceTable->Race->Circuit->CircuitName;
-        $country = $MRData->RaceTable->Race->Circuit->Location->Country;
-        $date = $MRData->RaceTable->Race->Date;
+        $season = (string)$MRData->RaceTable->attributes()->{'season'};
+        $round = (int)$MRData->RaceTable->attributes()->{'round'};
+        $raceName = (string)$MRData->RaceTable->Race->RaceName;
+        $circuitId = (string)$MRData->RaceTable->Race->Circuit->attributes()->{'circuitId'};
+        $circuitName = (string)$MRData->RaceTable->Race->Circuit->CircuitName;
+        $country = (string)$MRData->RaceTable->Race->Circuit->Location->Country;
+        $date = (string)$MRData->RaceTable->Race->Date;
 
         $race = new race($season,$round,$raceName,$circuitId, $circuitName, $country, $date);
 
-        if(!in_array($race, $this->races))
+        for ($i = 0; $i < count($this->races); $i++)
         {
-            array_push($this->races, $race);
-        }
-        else
-        {
-            echo "Already in array";
-            return;
+            if ($this->races[$i]->getRound() == $round)
+            {
+                return;
+            }
         }
 
+        array_push($this->races, $race);
 
+        //Getting driver data
 
 
 
         foreach($MRData->RaceTable->Race->ResultsList->Result as $Result)
         {
-            $permanentNumber =  $Result->Driver->PermanentNumber;
-            $points;
-            $code;
-            $givenName;
-            $familyName;
-            $dateOfBirth;
-            $nationality;
-            $driverId;
-            $constructorName;
+            $position = (int)$Result->attributes()->{'position'};
+            $points = (int)$Result->attributes()->{'points'};
+            $permanentNumber =  (int)$Result->Driver->PermanentNumber;
+            $driverId = (string)$Result->Driver->attributes()->{'driverId'};
+            $code = (string)$Result->Driver->attributes()->{'code'};
+            $givenName = (string)$Result->Driver->GivenName;
+            $familyName = (string)$Result->Driver->FamilyName;
+            $dateOfBirth = (string)$Result->Driver->DateOfBirth;
+            $nationality = (string)$Result->Driver->Nationality;
+            $constructorId = (string)$Result->Constructor->attributes()->{'constructorId'};
+            $constructorName = (string)$Result->Constructor->Name;
+            $constructorNationality = (string)$Result->Constructor->Nationality;
+            $fastestLapRank = (int)$Result->FastestLap->attributes()->{'rank'};
+            $fastestLapTime = (string)$Result->FastestLap->Time;
 
-            echo $permanentNumber . PHP_EOL;
+            $constructorAlreadyExists = false;
+            $driverAlreadyExists = false;
+            $driverConstructor = null;
+            $currentDriver = null;
+
+            for ($i = 0; $i < count($this->constructors); $i++)
+            {
+                if ($this->constructors[$i]->getConstructorId() == $constructorId)
+                {
+                    $constructorAlreadyExists = true;
+                    $driverConstructor = $this->constructors[$i];
+                }
+            }
+
+            if (!$constructorAlreadyExists)
+            {
+                $driverConstructor = new constructor($constructorId, $constructorName, $constructorNationality);
+                array_push($this->constructors, $driverConstructor);
+            }
+
+            $race->addConstructor($driverConstructor);
+
+            for ($i = 0; $i < count($this->drivers); $i++)
+            {
+                if ($this->drivers[$i]->getDriverId() == $driverId)
+                {
+                    $currentDriver = $this->drivers[$i];
+                    if ($this->drivers[$i]->getConstructor() != $driverConstructor)
+                    {
+                        $this->drivers[$i]->changeConstructor($driverConstructor);
+                    }
+                    $this->drivers[$i]->increasePoints($points);
+                    $driverAlreadyExists = true;
+                }
+            }
+
+            if(!$driverAlreadyExists)
+            {
+                $currentDriver = new driver($permanentNumber, $points, $code, $givenName, $familyName, $dateOfBirth, $nationality, $driverId, $driverConstructor);
+                array_push($this->drivers, $currentDriver);
+            }
+
+            $race->addDriver($currentDriver);
+
+            $raceResult = new raceResult($currentDriver, $driverConstructor, $position, $points, $fastestLapRank, $fastestLapTime);
+
+            $race->addRaceResult($raceResult);
+
+            if ($fastestLapRank == 1)
+            {
+                $race->setFastestLapTime($fastestLapTime, $currentDriver);
+            }
         }
+    }
 
-
+    public function printAllRaceResults()
+    {
+        for ($i = 0; $i < count($this->races); $i++)
+        {
+            $r = $this->races[$i]->getRaceResults();
+            for ($j = 0; $j < count($r); $j++)
+            {
+                $r[$j]->printRaceResult();
+            }
+        }
     }
 
     public function addDriver($driver)
