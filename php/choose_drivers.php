e61ab2edf860c98275a8b2f0c2e74930185ad1a6
Stian Onarheim <43683888+Feqzz@users.noreply.github.com>
2019-12-17 10:17:21 +0100

    Can now get drivers from database

--- php/choose_drivers.php
+++ php/choose_drivers.php
@@ -1,5 +1,36 @@
 <?php
 
+require_once("dbh.php");
+$link = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($link->connect_error)
+{
+    die("Connection failed " . $link->connect_error);
+}
 
+//Getting drivers from database
+$drivers_array = array();
+
+$resource = $link->query("SELECT * FROM drivers");
+while ($row = $resource->fetch_assoc())
+{
+    $permanent_number = "{$row['permanent_number']}";
+    $points = "{$row['points']}";
+    $code = "{$row['code']}";
+    $price = "{$row['price']}";
+    $given_name = "{$row['given_name']}";
+    $family_name = "{$row['family_name']}";
+    $date_of_birth = "{$row['date_of_birth']}";
+    $nationality = "{$row['nationality']}";
+    $driver_id = "{$row['driver_id']}";
+    $constructor_id = "{$row['constructor_id']}";
+    $season = "{$row['season']}";
+
+    $driver = new driver($permanent_number, $points, $code, $given_name, $family_name, $date_of_birth, $nationality, $driver_id, $season);
+    $driver->set_constructor_id($constructor_id);
+    array_push($drivers_array, $driver);
+}
+
+
+mysqli_close($link);
 
 ?>
