2fe080ff18b5487f551ecd1a350d1e4e67424068
Stian Onarheim <43683888+Feqzz@users.noreply.github.com>
2019-12-19 20:44:40 +0100

    Added a buy and sell function that works with the database.

--- php/player.php
+++ php/player.php
@@ -24,6 +24,18 @@ class player
             ";
 
         mysqli_query($link, $query);
+
+        $resource = $link->query("SELECT * FROM players WHERE id='$id'");
+        while ($row = $resource->fetch_assoc())
+        {
+            $this->money = "{$row['money']}";
+            $this->points = "{$row['points']}";
+            $this->driver_one = "{$row['driver_one']}";
+            $this->driver_two = "{$row['driver_two']}";
+            $this->driver_three = "{$row['driver_three']}";
+            $this->driver_four = "{$row['driver_four']}";
+            $this->driver_five = "{$row['driver_five']}";
+        }
         mysqli_close($link);
     }
 
@@ -34,20 +46,189 @@ class player
 
     public function add_driver($driver)
     {
-        if (!$driver->get_price() > $this->money) return false;
+        if (($driver->get_price() > $this->money))
+            return;
+
+        require_once("dbh.php");
+        $link = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if ($link->connect_error)
+        {
+            die("Connection failed " . $link->connect_error);
+        }
+
         $this->money -= $driver->get_price();
-        array_push($this->drivers, $driver);
-        return true;
+
+        $query = "";
+        $money_query =
+            "
+                UPDATE players
+                SET
+                    money = '$this->money'
+                WHERE
+                    id = '$this->id';
+            ";
+
+        if (!$this->driver_one)
+        {
+            $this->driver_one = $driver->get_driver_id();
+            $query =
+                "
+                    UPDATE players
+                    SET
+                        driver_one = '$this->driver_one'
+                    WHERE
+                        id = '$this->id'
+                ";
+        }
+        elseif(!$this->driver_two)
+        {
+            $this->driver_two = $driver->get_driver_id();
+            $query =
+                "
+                    UPDATE players
+                    SET
+                        driver_two = '$this->driver_two'
+                    WHERE
+                        id = '$this->id'
+                ";
+        }
+        elseif(!$this->driver_three)
+        {
+            $this->driver_three = $driver->get_driver_id();
+            $query =
+                "
+                    UPDATE players
+                    SET
+                        driver_three = '$this->driver_three'
+                    WHERE
+                        id = '$this->id'
+                ";
+        }
+        elseif(!$this->driver_four)
+        {
+            $this->driver_four = $driver->get_driver_id();
+            $query =
+                "
+                    UPDATE players
+                    SET
+                        driver_four = '$this->driver_four'
+                    WHERE
+                        id = '$this->id'
+                ";
+        }
+        elseif(!$this->driver_five)
+        {
+            $this->driver_five = $driver->get_driver_id();
+            $query =
+                "
+                    UPDATE players
+                    SET
+                        driver_five = '$this->driver_five'
+                    WHERE
+                        id = '$this->id'
+                ";
+        }
+        else
+        {
+            echo "You already have five drivers";
+        }
+
+        mysqli_query($link, $money_query);
+        mysqli_query($link, $query);
+        mysqli_close($link);
     }
 
-    public function remove_driver($driver)
+    public function sell_driver($driver)
     {
+        require_once("dbh.php");
+        $link = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if ($link->connect_error)
+        {
+            die("Connection failed " . $link->connect_error);
+        }
 
+        $sell_driver = true;
+        $query = "";
+        $money_query =
+            "
+                UPDATE players
+                SET
+                    money = '$this->money'
+                WHERE
+                    id = '$this->id';
+            ";
+        switch ($driver->get_driver_id())
+        {
+            case $this->driver_one:
+                $query =
+                    "
+                        UPDATE players
+                        SET
+                            driver_one = NULL
+                        WHERE
+                            id = '$this->id'
+                    ";
+                break;
+            case $this->driver_two:
+                $query =
+                    "
+                        UPDATE players
+                        SET
+                            driver_two = NULL
+                        WHERE
+                            id = '$this->id'
+                    ";
+                break;
+            case $this->driver_three:
+                $query =
+                    "
+                        UPDATE players
+                        SET
+                            driver_three = NULL
+                        WHERE
+                            id = '$this->id'
+                    ";
+                break;
+            case $this->driver_four:
+                $query =
+                    "
+                        UPDATE players
+                        SET
+                            driver_four = NULL
+                        WHERE
+                            id = '$this->id'
+                    ";
+                break;
+            case $this->driver_five:
+                $query =
+                    "
+                        UPDATE players
+                        SET
+                            driver_five = NULL
+                        WHERE
+                            id = '$this->id'
+                    ";
+                break;
+            default:
+                $sell_driver = false;
+                break;
+        }
+        if ($sell_driver)
+        {
+            $this->money += $driver->get_price();
+            mysqli_query($link, $query);
+            mysqli_query($link, $money_query);
+        }
+        mysqli_close($link);
     }
 
     private $id;
-    private $drivers = array();
     private $money;
     private $points;
+    private $driver_one;
+    private $driver_two;
+    private $driver_three;
+    private $driver_four;
+    private $driver_five;
 
 }
