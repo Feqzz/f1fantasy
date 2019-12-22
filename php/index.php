7a7844cc6885485de6fc99681d311277ea185dc8
Stian Onarheim <43683888+Feqzz@users.noreply.github.com>
2019-12-22 23:51:30 +0100

    Will now automatically get data from the next race. It will also lock the players drivers before the race, and update points after.

--- php/index.php
+++ php/index.php
@@ -1,19 +1,30 @@
 <?php
-
+require_once("dbh.php");
 require_once("mysql_tables.php");
 require_once("season.php");
 
-//$playerDriver = $_GET["Driver"];
+$round = 0;
+$year = 2019;
+$number_of_races = 20;
 
+$season = new season($year,$number_of_races);
 
+require_once("lock_player_drivers.php");
 
-$season = new season(2019,2);
+$link = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($link->connect_error)
+{
+    die("Connection failed " . $link->connect_error);
+}
 
-$season->get_race_data(1);
+$get_last_round = mysqli_query($link,"SELECT * FROM races WHERE season='$year' ORDER BY round DESC LIMIT 0, 1");
+$round_array = mysqli_fetch_array($get_last_round);
 
-$season->simulate_season();
+$round = $round_array['round'] + 1;
+mysqli_close($link);
 
+$season->get_race_data($round);
 
-
+require_once("update_player_points.php");
 ?>
 
