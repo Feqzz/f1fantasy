21be1c1f85286aa6ca6b88da19222ac04e019d7a
Stian Onarheim <43683888+Feqzz@users.noreply.github.com>
2019-12-25 14:30:31 +0100

    the result array has been sorted so the driver with the most points is shown on top.

--- php/show_race_result.php
+++ php/show_race_result.php
@@ -81,8 +81,8 @@ for ($i = 0; $i < count($drivers); $i++)
     }
 }
 array_multisort(array_map(function($element) {
-    return $element[0];
-}, $drivers), SORT_DESC, $race_results);
+    return $element[2];
+}, $race_results), SORT_DESC, $race_results);
 mysqli_close($link);
 ?>
 
